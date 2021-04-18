4d7bb5ecb2b16d7aff656230a9a3cb1455ab1e9e
corogit <66649016+corogit@users.noreply.github.com>
2021-04-18 18:48:48 +0900

    モダンJavaScript

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
-import "./styles.css";
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// constで定義したオブジェクトはプロパティーの変更が可能
+// const val4 = {
+//   name: "じゃけぇ",
+//   age: 28,
+// };
+// val4.name = "jak";
+// val4.address = "Hiroshima";
+// console.log(val4);
+
+// // constで定義した配列はプロパティーの変更が可能
+// const val5 = ['dog', 'cat'];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+// テンプレート文字列を用いた方法
+// const name = "じゃけぇ";
+// const age = 28;
+
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
